ef9fdacecd5ba99f8bf9a2e027549a9f902707de
Philip Jenvey <pjenvey@underboss.org>
2022-10-29 23:59:11 -0700

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,51 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//use async_trait::async_trait;
+use futures::future::LocalBoxFuture;
+
+type DbResult<T> = Result<T, std::io::Error>;
+type DbFuture<'a, T> = LocalBoxFuture<'a, DbResult<T>>;
+
+pub struct Info;
+pub struct Params;
+pub struct PostResult;
+
+pub trait Db<'a>: 'a {
+    fn begin(&self, opt: bool) -> DbFuture<'_, ()>;
+
+    fn post(&self, params: Params) -> DbFuture<'_, PostResult>;
+
+    fn info(&self) -> Info;
+}
+
+struct MysqlDb;
+
+impl MysqlDb {
+
+    async fn begin_async(&self, _opt: bool) -> DbResult<()> {
+        Ok(())
+    }
+
+    async fn post_async(&self, _params: Params) -> DbResult<PostResult> {
+        // XXX: run on blocking thread pool
+        Ok(PostResult)
+    }
+
+}
+
+impl<'a> Db<'a> for MysqlDb {
+    fn begin(&self, opt: bool) -> DbFuture<'_, ()> {
+        let db = self.clone();
+        Box::pin(async move { db.begin_async(opt).await })
+    }
+
+    fn post(&self, params: Params) -> DbFuture<'_, PostResult> {
+        let db = self.clone();
+        Box::pin(async move { db.post_async(params).await })
+    }
+
+
+    fn info(&self) -> Info {
+        Info
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +53,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn basic() {
     }
 }
